34a8df478f42e84d77f39e112fd0adbd8ff7d6a8
raffaelecheula <32555922+raffaelecheula@users.noreply.github.com>
2018-05-17 17:20:18 +0200

    Add files via upload

--- supercell.py
+++ supercell.py
@@ -4,118 +4,116 @@
 # Raffaele Cheula, LCCP, Politecnico di Milano, raffaele.cheula@polimi.it
 ################################################################################
 
-from __future__ import division, print_function
-from write_pwscf import PWscf
-from supercell_builder import *
+from __future__ import absolute_import, division, print_function
+import os
+from addict import Dict
+from ase import Atoms
+from ase.calculators.espresso import Espresso
+from supercell_builder import Bulk, Slab, Adsorbate, Vacancy
 
 ################################################################################
-#                                SUPERCELL
+# BUILD SUPERCELL
 ################################################################################
 
-CO = [['C', 0., 0., 0.], ['O', 0., 0., 1.]]
+CO = Atoms(('C', 'O'), ((0., 0., 0.), (0., 0., 1.2)))
 
-sc = Supercell()
+bulk_type         = 'fcc'
+input_bulk        = None
+elements          = 'Rh'
+lattice_constants = 3.863872
+kpts_bulk         = (12, 12, 12)
 
-sc.bulk_type         = 'fcc'
-sc.elements          = 'Rh'
-sc.lattice_constants = 3.83
+input_slab        = None
+miller_index      = '211'
+surface_vectors   = 'automatic'
+dimensions        = (2, 2)
+layers            = 4
+layers_fixed      = 3
+symmetry          = 'asymmetric'
+rotation_angle    = 'automatic'
+scale_kpts        = 'xy'
+adsorbates        = []
+vacancies         = []
+vacuum            = 10.
+sort_atoms        = True
 
-sc.input_slab        = None
-sc.miller_index      = '100'
-sc.surface_vectors   = 'automatic'
-sc.dimensions        = (2, 2)
+adsorbates.append(Adsorbate(CO, site = 'top', distance = 1.5))
 
-sc.layers            = 5
-sc.layers_fixed      = 4
-sc.symmetry          = None
-sc.cut_top           = None
-sc.cut_bottom        = None
-sc.vacuum            = 10.
+bulk = Bulk(bulk_type         = bulk_type,
+            input_bulk        = input_bulk,
+            elements          = elements,
+            lattice_constants = lattice_constants,
+            kpts_bulk         = kpts_bulk)
 
-sc.adsorbates        = (CO, 'top', 1, 1.5)
-sc.vacancies         = None
-sc.units             = 'final_cell'
-sc.break_sym         = False
-sc.rotation_angle    = 'automatic'
+slab = Slab(bulk              = bulk,
+            input_slab        = input_slab,
+            miller_index      = miller_index,
+            surface_vectors   = surface_vectors,
+            dimensions        = dimensions,
+            layers            = layers,
+            layers_fixed      = layers_fixed,
+            symmetry          = symmetry,
+            rotation_angle    = rotation_angle,
+            scale_kpts        = scale_kpts,
+            adsorbates        = adsorbates,
+            vacancies         = vacancies,
+            vacuum            = vacuum,
+            sort_atoms        = sort_atoms)
 
-sc.k_points          = [12, 12, 1]
-sc.scale_kp          = 'xy'
-sc.epsi              = 1e-4
-
-system   = sc.create_system()
-height   = sc.height
-k_points = sc.k_points
-
-################################################################################
-#                         QUANTUM ESPRESSO VARIABLES
-################################################################################
-
-qe = PWscf()
-
-# --- CONTROL -----------------------------------------------------------------
-
-qe.control.calculation           = 'relax'
-qe.control.restart_mode          = 'from_scratch'
-qe.control.forc_conv_thr         = 1.0e-3
-qe.control.etot_conv_thr         = 1.0e-4
-qe.control.max_seconds           = 1700
-qe.control.prefix                = 'pwscf'
-qe.control.outdir                = './tmp/'
-qe.control.nstep                 = 1000
-
-# --- SYSTEM ------------------------------------------------------------------
-
-qe.system.ecutwfc                = 35.
-qe.system.ecutrho                = 280.
-qe.system.occupations            = 'smearing'
-qe.system.smearing               = 'mv'
-qe.system.degauss                = 0.001
-qe.system.vdw_corr               = 'grimme-d3'
-#qe.system.input_dft              = 'vdw-df2'
-#qe.system.nspin                  = 2
-#qe.system.lda_plus_u             = True
-
-# --- LDA+U & MAGNETIZATION ---------------------------------------------------
-
-qe.lda_plus_u.u_dict             = {'Rh':3.5} # dictionary for U parameters
-qe.magnet.mag_dict               = {'Rh':1.} # dictionary for magnetization
-
-# --- ELECTRONS ---------------------------------------------------------------
-
-qe.electrons.conv_thr            = 1.0e-6
-qe.electrons.diagonalization     = 'david'
-qe.electrons.diago_david_ndim    = 2
-qe.electrons.mixing_beta         = 0.2
-qe.electrons.mixing_mode         = 'local-TF'
-qe.electrons.electron_maxstep    = 500
-
-# --- IONS --------------------------------------------------------------------
-
-qe.ions.ion_dynamics             = 'bfgs'
-
-# --- CELL --------------------------------------------------------------------
-
-qe.cell.cell_dynamics            = 'bfgs'
-qe.cell.cell_dofree              = 'z'
-
-# --- K POINTS ----------------------------------------------------------------
-
-qe.kpoints.nk                    = k_points
-qe.kpoints.type                  = 'automatic'
-
-# --- ATOMIC POSITIONS --------------------------------------------------------
-
-qe.atomic_positions.type         = 'angstrom'
+atoms = slab.atoms
+constraints = slab.constraints
+kpts = slab.kpts
+koffset = slab.koffset
 
 ################################################################################
-#                               WRITE OUTPUTS
+# WRITE QUANTUM ESPRESSO INPUT
 ################################################################################
 
-qe.atoms = system
-qe.write_input('pw.inp')
-system.write('pw.xsf')
-system.write('pw.traj')
+input_data = qe = Dict()
+
+qe.outdir           = 'tmp'
+qe.forc_conv_thr    = 1e-3
+qe.etot_conv_thr    = 1e-4
+qe.dipfield         = True
+
+qe.ecutwfc          = 35.
+qe.ecutrho          = 280.
+qe.occupations      = 'smearing'
+qe.smearing         = 'mv'
+qe.degauss          = 0.001
+
+qe.conv_thr         = 1e-6
+qe.diagonalization  = 'david'
+qe.diago_david_ndim = 2
+qe.mixing_beta      = 0.2
+qe.mixing_mode      = 'local-TF'
+qe.electron_maxstep = 500
+
+pseudos = {'Rh': 'Rh.UPF',
+           'C' : 'C.UPF',
+           'O' : 'O.UPF',
+           'H' : 'H.UPF'}
+
+calc = Espresso(input_data       = input_data,
+                pseudopotentials = pseudos,
+                kpts             = kpts,
+                koffset          = koffset,
+                calculation      = 'relax',
+                restart_mode     = 'from_scratch',
+                max_seconds      = 1700)
+
+calc.write_input(atoms)
+os.rename('espresso.pwi', 'pw.inp')
 
 ################################################################################
-#                                    END
+# RUN QUANTUM ESPRESSO
+################################################################################
+
+run_qe = False
+
+if run_qe is True:
+    os.system("run pw -fs -n=2 -t=0.5")
+
+################################################################################
+# END
 ################################################################################
